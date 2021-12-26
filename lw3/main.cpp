7a4141b6120a2bece80b799e75646971a7ad152a
Igor Mayshanov <pogopios@gmail.com>
2021-12-26 20:57:47 +0300

    почти готово

--- lw3/main.cpp
+++ lw3/main.cpp
@@ -26,32 +26,34 @@ const int numberOfDirections = 8;
 const int directionsX[numberOfDirections] = {1, -1, 2, 2, 1, -1, -2, -2};
 const int directionsY[numberOfDirections] = {-2, -2, 1, -1, 2, 2, 1, -1};
 
-int getOffset(int x, int y, int fieldSize)
+void printMatrix(int **matrix, int fieldSize)
 {
-    return x + y * fieldSize;
+    for (int i = 0; i < fieldSize; i++)
+    {
+        for (int j = 0; j < fieldSize; j++)
+        {
+            printf("%3d", matrix[j][i]);
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
 
-void findPath(int fieldSize, int x, int y, vector<char> playingField, int **visited, int **path, queue<int> &plan);
+void findPath(int fieldSize, int x, int y, vector<vector<char>> playingField, int **visited, int **path, queue<int> &plan);
 
-void restorePath(int fieldSize, int x_start, int y_start, int x_end, int y_end, vector<char> &playingField, int **path); /* восстановление пути*/
+void restorePath(int fieldSize, int x_start, int y_start, int x_end, int y_end, vector<vector<char>> &playingField, int **path); /* восстановление пути*/
 
-void PrintVector(vector<char> Vector);
-
-void ReadLineFromFile(ifstream &FileIn, vector<char> &playingField);
-
-vector<char> ReadFileInVector();
+void printVector2(int fieldSize, vector<vector<char>> playingField);
 
 int main()
 {
     int fieldSize;
     scanf("%d", &fieldSize);
-    vector<char> playingField;
-
+    vector<vector<char>> playingField(fieldSize, vector<char>(fieldSize));
     int x_start, y_start, x_end, y_end, x, y;
     queue<int> cellVisitQueue;
     int **visited = new int *[fieldSize];
     int **path = new int *[fieldSize];
-    cout << fieldSize << endl;
     for (int y = 0; y < fieldSize; y++)
     {
         visited[y] = new int[fieldSize]; /* массив для хранения информации о посещении клеток*/
@@ -60,9 +62,8 @@ int main()
         {
             visited[y][x] = 0;
             path[y][x] = -1;
-            int offset = getOffset(x, y, fieldSize);
-            scanf(" %c", &playingField[offset]);
-            if (playingField[offset] == STARTING_POSITION_SYMBOL)
+            scanf(" %c", &playingField[y][x]);
+            if (playingField[y][x] == STARTING_POSITION_SYMBOL)
             { /* находим начало пути*/
                 x_start = x;
                 y_start = y;
@@ -70,14 +71,21 @@ int main()
                 cellVisitQueue.push(y); /* в план посещения */
                 path[x][y] = 1;
             }
-            else if (playingField[offset] == END_POSITION_SYMBOL)
+            else if (playingField[y][x] == END_POSITION_SYMBOL)
             { /* находим конечную точку */
                 x_end = x;
                 y_end = y;
             }
         }
-        printf("\n");
     }
+    cout << "printVector2" << endl;
+    printVector2(fieldSize, playingField);
+    cout << "Path" << endl;
+    printMatrix(path, fieldSize);
+    cout << "Visited" << endl;
+    printMatrix(visited, fieldSize);
+    cout << "x_start: " << x_start << "y_start: " << y_start << endl;
+    cout << "x_end: " << x_end << "y_end: " << y_end << endl;
     while (!cellVisitQueue.empty())
     {
         x = cellVisitQueue.front();
@@ -94,20 +102,16 @@ int main()
     {
         cout << "Yes" << endl;
         restorePath(fieldSize, x_start, y_start, x_end, y_end, playingField, path);
-        for (int i = 0; i < fieldSize; i++)
-        {
-            for (int j = 0; j < fieldSize; j++)
-            {
-                int offset = getOffset(j, i, fieldSize);
-                printf("%c", playingField[offset]);
-            }
-            printf("\n");
-        }
+        cout << "Path" << endl;
+        printMatrix(path, fieldSize);
+        cout << "Visited" << endl;
+        printMatrix(visited, fieldSize);
+        printVector2(fieldSize, playingField);
     }
     return 0;
 }
 
-void findPath(int fieldSize, int x, int y, vector<char> playingField, int **visited, int **path, queue<int> &plan)
+void findPath(int fieldSize, int x, int y, vector<vector<char>> playingField, int **visited, int **path, queue<int> &plan)
 {
     int offset = 0;
     int ix, iy;
@@ -118,9 +122,8 @@ void findPath(int fieldSize, int x, int y, vector<char> playingField, int **visi
         {
             ix = x + directionsX[i];
             iy = y + directionsY[i];
-            offset = getOffset(ix, iy, fieldSize);
             if (ix < fieldSize && ix >= 0 && !visited[ix][iy] &&
-                (playingField[offset] == FREE_CELL_SYMBOL || playingField[offset] == END_POSITION_SYMBOL))
+                (playingField[ix][iy] == FREE_CELL_SYMBOL || playingField[ix][iy] == END_POSITION_SYMBOL))
             {
                 path[ix][iy] = path[x][y] + 1;
                 plan.push(ix);
@@ -139,11 +142,10 @@ bool checkOutOfBorder(int x, int y, int fieldSize)
         return false;
 }
 
-void restorePath(int fieldSize, int x_start, int y_start, int x_end, int y_end, vector<char> &playingField, int **path) /* восстановление пути*/
+void restorePath(int fieldSize, int x_start, int y_start, int x_end, int y_end, vector<vector<char>> &playingField, int **path) /* восстановление пути*/
 {
     int x = x_end;
     int y = y_end;
-    int offset = getOffset(x, y, fieldSize);
     char pathMarker = '@';
     int ix, iy;
     while (path[x][y] != path[x_start][y_start] + 1)
@@ -156,54 +158,21 @@ void restorePath(int fieldSize, int x_start, int y_start, int x_end, int y_end, 
             {
                 x = ix;
                 y = iy;
-                offset = getOffset(x, y, fieldSize);
-                playingField[offset] = pathMarker;
+                cout << "x_= " << x << " y_= " << y << " " << path[x][y] << endl;
+                playingField[y][x] = pathMarker;
             }
         }
     }
 }
 
-void PrintVector(vector<char> Vector)
+void printVector2(int fieldSize, vector<vector<char>> playingField)
 {
-    for (int i = 0; i < Vector.size(); i++)
-        std::cout << Vector[i] << std::endl;
-}
-
-void ReadLineFromFile(ifstream &FileIn, vector<char> &playingField)
-{
-    string s;
-    getline(FileIn, s);
-    std::stringstream ss;
-    ss.str(s);
-    char c2;
-    string s2;
-    int Number;
-    while (ss >> s2)
+    for (int i = 0; i < fieldSize; i++)
     {
-        c2 = *s2.c_str();
-        playingField.push_back(c2);
-    }
-}
-
-vector<char> ReadFileInVector()
-{
-    setlocale(LC_ALL, "ru");
-    string pathIn = "input.txt";
-    vector<char> playingField;
-    ifstream FileIn;
-    FileIn.open(pathIn);
-    if (!FileIn.is_open())
-    {
-        std::cout << "file open error" << std::endl;
-    }
-    else
-    {
-        std::cout << "file open" << std::endl;
-        while (!FileIn.eof())
+        for (int j = 0; j < fieldSize; j++)
         {
-            ReadLineFromFile(FileIn, playingField);
+            printf("%c", playingField[i][j]);
         }
+        printf("\n");
     }
-    FileIn.close();
-    return playingField;
 }
